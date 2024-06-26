aff8fa32179aca97dfc52ff72962f1df8da91c17
arunmishra11 <156019606+arunmishra11@users.noreply.github.com>
2024-06-25 20:59:20 -0500

    updated thoughtContoller to get all the thoughts and single thought by id

---
+++ controllers/thoughtController.js
@@ -0,0 +1,20 @@
+const { User, Thought } = require("../models");
+
+module.exports = {
+  // Get all thoughts
+  getThought(req, res) {
+    Thought.find({})
+      .then((thought) => res.json(thought))
+      .catch((err) => res.status(500).json(err));
+  },
+  // get single thought
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .select("-__v")
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No Thought find with this ID!" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
